0f10c5c14fc400d97380cf2fd0eb236972495bbb
Dustin Gelelegonya <github@gelegonya.com>
2025-06-09 04:25:11 -0400

    testing

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 # this will create backups of all dotfiles that might be replaced.
+git config --global init.defaultBranch master
+git config --global user.name "Dustin Gelelegonya"
+git config --global user.email "github@gelegonya.com"
