2fa9af71167422aa48d16ce6a7a0ddf53dbd3eb5
Dustin Gelelegonya <github@gelegonya.com>
2025-06-09 05:12:34 -0400

    working nicely

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Dotfiles symlinked on my machine
 
+`brew install stow`
+
 ### Install with stow:
 ```bash
-stow --dotfiles .
+stow --dotfiles . && exec zsh
 ```
+
+Ref: https://github.com/omerxx/dotfiles
+Ref: duck://player/y6XCebnB9gs
--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # this will create backups of all dotfiles that might be replaced.
-git config --global init.defaultBranch master
-git config --global user.name "Dustin Gelelegonya"
-git config --global user.email "github@gelegonya.com"
+
+#mv ~/Music ~/Music.bak
+stow --dotfiles . && exec zsh
