adc5bfc88b836dafbadc26efba92d523a3e9d896
Dustin Gelegonya <187451552+dustingelegonya@users.noreply.github.com>
2025-06-14 19:03:14 -0400

    Update run-init.sh

--- dot-bin/run-init.sh
+++ dot-bin/run-init.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env zsh
+
+echo
 # https://docs.brew.sh/Installation#multiple-installations-unsupported
 DOTFILES_DIR=$HOME/..dot-files
 DOTFILES=$DOTFILES_DIR
@@ -10,7 +12,9 @@ mkdir $HOMEBREW_INSTALL_PATH
 curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOMEBREW_INSTALL_PATH
 eval "$($HOMEBREW_INSTALL_PATH/bin/brew shellenv)"
 which brew
-brew install stow #starship tree --cask ghostty
+brew install stow #starship --cask ghostty
 stow --dotfiles --dir=$DOTFILES .
 #source $HOME/$DOTFILES/dot-zshrc
 exec zsh
+cp ~/.fonts/Agave/*.ttf ~/Library/Fonts/
+
