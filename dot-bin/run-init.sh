6401319d07ed59057f8ef4e527b7e4087d3f49a5
Dustin Gelegonya <187451552+dustingelegonya@users.noreply.github.com>
2025-06-14 18:52:09 -0400

    Update run-init.sh

--- dot-bin/run-init.sh
+++ dot-bin/run-init.sh
@@ -10,7 +10,7 @@ mkdir $HOMEBREW_INSTALL_PATH
 curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOMEBREW_INSTALL_PATH
 eval "$($HOMEBREW_INSTALL_PATH/bin/brew shellenv)"
 which brew
-brew install stow starship tree --cask ghostty
+brew install stow #starship tree --cask ghostty
 stow --dotfiles --dir=$DOTFILES .
 #source $HOME/$DOTFILES/dot-zshrc
 exec zsh
