51bc2051b1c61503dea6a08425c00bec1678c4bb
Dustin Gelegonya <187451552+dustingelegonya@users.noreply.github.com>
2025-06-14 20:08:18 -0400

    Update configuration.x86.nix

--- dot-config/nix-darwin/configuration.x86.nix
+++ dot-config/nix-darwin/configuration.x86.nix
@@ -1,1 +1,49 @@
+# configuration.nix
+{ pkgs, ... }: {
+  environment.systemPackages = [
+    pkgs.vim
+    pkgs.direnv
+    pkgs.htop
+    pkgs.git
+    pkgs.wget
+    pkgs.curl
+  ];
+
   nixpkgs.hostPlatform = "x86_64-darwin";
+  nix.settings.experimental-features = "nix-command flakes";
+
+  nix.linux-builder.enable = true;
+  programs.zsh.enable = true;
+
+  # Passwordless sudo for your user
+  security.sudo.extraConfig = ''
+    dust ALL=(ALL) NOPASSWD: ALL
+  '';
+
+  system.primaryUser = "dust";
+
+  system.defaults = {
+    alf.globalstate = 1;
+    smb.ServerDescription = "alpha";
+    dock.autohide = true;
+    dock.mru-spaces = false;
+    finder.AppleShowAllExtensions = true;
+    finder.FXPreferredViewStyle = "clmv";
+    loginwindow.LoginwindowText = "Jelly Labs Limited";
+    screencapture.location = "~/Desktop";
+    screensaver.askForPasswordDelay = 10;
+  };
+
+  # Homebrew configuration
+  homebrew.enable = true;
+  homebrew.casks = [
+    "wireshark"
+    "google-chrome"
+  ];
+  homebrew.brews = [
+    "imagemagick"
+    "ffmpeg"
+    "exiftool"
+    "stow"
+  ];
+}
